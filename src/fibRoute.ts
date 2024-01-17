4643be4ef9030abdcb60621f14d05601e56de280
sravyayenumula <156870658+sravyayenumula@users.noreply.github.com>
2024-01-16 22:40:53 -0500

    Update fibRoute.ts

--- src/fibRoute.ts
+++ src/fibRoute.ts
@@ -6,10 +6,10 @@ export default (req: Request, res: Response) => {
   const { num } = req.params;
 
   const fibN = fibonacci(parseInt(num));
-  let result = fibonacci(${num}) is ${fibN};
+  let result = `fibonacci(${num}) is ${fibN}`;
 
   if (fibN < 0) {
-    result = fibonacci(${num}) is undefined;
+    result = `fibonacci(${num}) is undefined`;
   }
 
   res.send(result);
