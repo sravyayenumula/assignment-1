d95828ac1ef132c5ae553594003be84c3a35ac13
sravyayenumula <156870658+sravyayenumula@users.noreply.github.com>
2024-01-16 22:32:51 -0500

    Update fibRoute.ts

--- src/fibRoute.ts
+++ src/fibRoute.ts
@@ -1,15 +1,16 @@
 // Endpoint for querying the fibonacci numbers
 
+import { Request, Response } from "express";
 import fibonacci from "./fib";
 
-export default (req, res) => {
+export default (req: Request, res: Response) => {
   const { num } = req.params;
 
   const fibN = fibonacci(parseInt(num));
-  let result = `fibonacci(${num}) is ${fibN}`;
+  let result = fibonacci(${num}) is ${fibN};
 
   if (fibN < 0) {
-    result = `fibonacci(${num}) is undefined`;
+    result = fibonacci(${num}) is undefined;
   }
 
   res.send(result);
